cb9b8fcc0a70b5e6aa098ab949137acf985fa020
Pedro Mello <pedro@mushdigital.com>
2015-08-27 02:14:33 +1000

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,6 @@ module.exports = {
     },
     module: {
         loaders: [
-            {test: /\.js$/,exclude: [node_modules_dir],loader: 'babel'},
             {test: /\.css$/, loader: "style!css"},
             // the file-loader emits files. 
             {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff"},
